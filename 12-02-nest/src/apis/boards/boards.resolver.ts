dab28db19316686cf08427df857024ec98774e0c
scw3812 <scw3812@naver.com>
2023-01-25 15:52:01 +0900

    nest graphql

---
+++ 12-02-nest/src/apis/boards/boards.resolver.ts
@@ -0,0 +1,13 @@
+import { Query, Resolver } from '@nestjs/graphql';
+
+import { BoardService } from './boards.service';
+
+@Resolver()
+export class BoardResolver {
+  constructor(private readonly boardService: BoardService) {}
+
+  @Query(() => String)
+  getHello() {
+    return this.boardService.getHello();
+  }
+}
---
+++ 12-02-nest/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { BoardModule } from './apis/boards/boards.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    BoardModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
