d29b7e834796f472d2b0d63a0b8c84d72620a799
scw3812 <scw3812@naver.com>
2023-01-31 15:20:21 +0900

    bcrypt

--- 12-02-nest/src/apis/users/user.service.ts
+++ 12-02-nest/src/apis/users/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserInput } from './dto/createUser.input';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -12,12 +13,18 @@ export class UserService {
   ) {}
 
   async create(createUserInput: CreateUserInput): Promise<User> {
+    const { email, password, ...rest } = createUserInput;
     const user = await this.userRepository.findOne({
       email: createUserInput.email,
     });
     if (user) {
       throw new ConflictException('이미 등록된 이메일입니다');
     }
-    return await this.userRepository.save(createUserInput);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return await this.userRepository.save({
+      email,
+      password: hashedPassword,
+      ...rest,
+    });
   }
 }
